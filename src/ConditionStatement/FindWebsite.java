b1d2a1041fa68078774d87b6d5bb9f2b3f804b7b
samyak jain <sj73511205@gmail.com>
2022-04-06 14:14:28 +0000

    conditions

---
+++ src/ConditionStatement/FindWebsite.java
@@ -0,0 +1,8 @@
+package ConditionStatement;
+import java.util.Scanner;
+public class FindWebsite {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner (System.in);
+
+    }
+}
--- src/com/company/ConditionStatement.java
+++ src/com/company/ConditionStatement.java
@@ -2,6 +2,25 @@ package com.company;
 
 public class ConditionStatement {
     public static void main(String[] args) {
-
+/*if,else, leader
+Multiple codition are used when
+if, else if, else if, else if, else leader
+switch case are used when tab a button click on the result this is used for multiple condition
+systax
+switch(variable){
+case1: statement;
+break;
+case2: statement;
+break;
+default: statement;
+}
+ */
+        int a = 18;
+                if(a==19){
+                    System.out.println("this is  correct value");
+                }
+                else{
+                    System.out.println("this is  not correct value");
+                }
     }
 }
