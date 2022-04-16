7d9aaa1f3871ff75be6cf3e48aed4bc7d221533d
samyak jain <sj73511205@gmail.com>
2022-04-16 15:45:10 +0000

    TablePrint

--- src/loops/TablePrint.java
+++ src/loops/TablePrint.java
@@ -2,10 +2,12 @@ package loops;
 import java.util.Scanner;
 public class TablePrint {
     public static void main(String[] args) {
-        System.out.println("Enter your number :");
+        System.out.println("Enter 2 table:");
         Scanner Sc = new Scanner(System.in);
         int n =Sc.nextInt();
-        for ( int i=1; i<=11; i++){
+       // for ( int i=1; i<=11; i++){
+        // reverse order
+        for (int i =10; i>=1; i--){
             System.out.println("printed two table "+(i*n));
         }
 
