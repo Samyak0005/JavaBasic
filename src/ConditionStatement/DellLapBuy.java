a02d1a80aa6c047bc31cd255f964b722c30d1199
samyak jain <sj73511205@gmail.com>
2022-04-19 20:37:09 +0000

    conditional statement buy lap

---
+++ src/ConditionStatement/DellLapBuy.java
@@ -0,0 +1,16 @@
+package ConditionStatement;
+
+public class DellLapBuy {
+    public static void main(String[] args) {
+        String laptop = "Dell";
+        int Ram = 8;
+        int Hd = 512;
+        // & AND operator are used for when all conditions are true
+        if(laptop=="Dell"& Ram>4& Hd>128){
+            System.out.println("this is perfect laptop for me");
+        }
+        else{
+            System.out.println("this is not good for me");
+        }
+    }
+}
