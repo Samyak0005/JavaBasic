96843dea74067cc33bfbd8d35a14025549276bf9
samyak jain <sj73511205@gmail.com>
2022-04-20 03:05:58 +0000

    display array

---
+++ src/Arrays/DisplayArray.java
@@ -0,0 +1,14 @@
+package Arrays;
+//import java.util.Scanner;
+public class DisplayArray {
+    public static void main(String[] args) {
+  //      Scanner sc = new Scanner(System.in);
+    //    int marks = sc.nextInt();
+        int [] marks = {34,45,64,67,70};
+      //  System.out.println(marks.length);
+       // System.out.println(marks[0]);
+        for(int i =0; i<=marks.length; i++ ){
+            System.out.println(marks[i]);
+        }
+    }
+}
