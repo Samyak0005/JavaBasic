b3b915ec27203cff9f7f67d3e470939749363c3f
samyak jain <sj73511205@gmail.com>
2022-04-24 16:12:43 +0000

    pratice set2 swap the value

---
+++ src/PracticeSet2/Swap_Two_Num.java
@@ -0,0 +1,22 @@
+package PracticeSet2;
+
+public class Swap_Two_Num {
+    public static void main(String[] args) {
+       // System.out.println("swapping two number/n using three variables");
+        System.out.println("swapping two number/n using two variables");
+     /*   int a = 8;
+        int b = 19;
+        int c;
+        c=b;
+        b=a;
+        a=c; */
+        int a = 2;
+        int b = 6;
+        a = a+b;
+        b = a-b;
+        a = a-b;
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }
+}
